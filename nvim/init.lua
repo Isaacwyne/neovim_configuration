b3f40e016f29c57722adffcbb765ddacadfd5a52
Isaac Ruvuza <ruvuzaisaac@gmail.com>
2022-07-15 13:07:20 +0300

    added impatient to start plugins after nvim starts & added function to return errors incase plugins doesn't start

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,11 +1,29 @@
-require"core.mappings"
-require"core.options"
-require"core.plugins"
+local present, impatient = pcall(require, "impatient")
+if present then
+    impatient.enable_profile()
+end
 
--- plugin configurations
-require"configs.bufferline"
-require"configs.cmp"
-require"configs.indentline"
-require"configs.nvimtree"
-require"configs.others"
-require"configs.statusline"
+for _, source in ipairs {
+    "core.options",
+    "core.mappings",
+    "core.plugins",
+    
+    -- plugin configurations
+    "plugins.configs.autopairs",
+    "plugins.configs.better_escape",
+    "plugins.configs.blankline",
+    "plugins.configs.bufferline",
+    "plugins.configs.cmp",
+    "plugins.configs.colorizer",
+    "plugins.configs.comment",
+    "plugins.configs.gitsigns",
+    "plugins.configs.indentline",
+    "plugins.configs.nvimtree",
+    "plugins.configs.statusline",
+    "plugins.configs.telescope",
+} do
+    local status_ok, fault = pcall(require, source)
+    if not status_ok then
+        vim.api.nvim_err_writeln("Failed to load " .. source .. "\n\n" .. fault)
+    end
+end
